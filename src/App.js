bb50a7f6701d00d202c56152939b119738e59e95
David_Sneek <davidjfk@hotmail.com>
2022-05-30 11:29:09 +0200

    Hello World. This will be my Winc Academy Assignment Songsaver.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world. This will be my Winc Academy Assignment Songsaver</h1>
     </div>
   );
 }
